77e12f659a48804e9a2bfe3cfdb66b50f6faf9d1
sneha-tiwari06 <tiwarisneha491@gmail.com>
2024-07-31 18:33:41 +0530

    student details added

--- index.js
+++ index.js
@@ -68,6 +68,16 @@ app.get('/api/students', async (req, res) => {
         res.status(500).json({ message: 'Failed to fetch students', error });
     }
 });
+app.get('/api/students/:id', async (req, res) => {
+    try {
+        const student = await Student.findById(req.params.id);
+        if (!student) return res.status(404).send('Student not found');
+        res.json(student);
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
